abca32086a54f7674b32c74d1c56b2f5a6d9d892
atulya srivastava <vns.atulyasrivastava@gmail.com>
2025-12-11 20:59:58 +0530

    feat: add speedinsights by vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthProvider from '../context/AuthProvider';
 import { Toaster } from 'sonner';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
